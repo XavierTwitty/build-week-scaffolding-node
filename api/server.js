7c9271305649bc71595791ec077be940077d496e
XavierTwitty <x.twitty@gmail.com>
2022-04-04 12:18:10 -0400

    routers

--- api/server.js
+++ api/server.js
@@ -2,11 +2,18 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router')
+
 const server = express()
 
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
